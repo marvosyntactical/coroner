bff8c3a52697a94d02097d5536c9f9103af49da3
marvosyntactical <koss@cl.uni-heidelberg.de>
2020-04-02 01:30:19 +0200

    did stuuuf

--- psytrance.py
+++ psytrance.py
@@ -1,23 +1,108 @@
 from pytrends.request import TrendReq
-from trendsdata import load_kws, langs, topics, kw_tmpl, ext, read_proxies
+from trendsdata import load_kws, langs, topics, kw_tmpl, ext 
 import os
+import json
 from typing import List, Optional
 import numpy as np
+import pandas as pd
+from functools import wraps
+import warnings
 
 
 # mock data
 topic = "finance"
+countries = ["germany, united kingdom"]
 
 # log to path
+global log_path, country_info_path, countriesINFO
+
 log_path = "kw-trends/"
+country_info_path = "langcountrycodes"
+countriesINFO = country_info_path+"countriesINFO.pd"
 
-def state_lang(country: str="") -> str:
-    """
-    Returns language spoken in a country
-    """
-    raise NotImplementedError
 
-def update_trends(proxies:List[str]=["http://179.108.169.71:8080"]) -> None:
+
+def country_lang(country: str="", geo_lkp_file: str=countriesINFO, proximity=5) -> str:
+    """
+
+    converts country str (MUST be in geo_lkp_file["Country"] to language) 
+     to list of languages spoken in that country
+    
+
+    :param country: country string    
+    Returns language id spoken in a country
+    """
+
+    #preproc
+    country = country.lower()
+
+    df = pd.read_pickle(lkp_file) 
+
+    index = df.index[df["Country"]==country]
+    country_row = df.iloc[index]
+    languages = country_row["Languages"]
+    try:
+        lang_codes = languages.item() #if succeeds, found exactly one matching country
+    except ValueError:
+        countries = df["Country"].to_list()
+
+        
+        lang_codes = None #failed, string didnt match any country
+        warnings.warn(f"Couldnt find country={country} in country info file={geo_lkp_file};\n\
+            here 5 closest matches: {close[:proximity-1]}")
+        
+
+    return lang_codes
+
+def distanceFrom(s1, *args):
+    #how to return copy of levenshteindist func wherein
+    #s1 is specified???
+        
+    def levenshteinDistance(s1, s2):
+        if len(s1) > len(s2):
+            s1, s2 = s2, s1
+
+        distances = range(len(s1) + 1)
+        for i2, c2 in enumerate(s2):
+            distances_ = [i2+1]
+            for i1, c1 in enumerate(s1):
+                if c1 == c2:
+                    distances_.append(distances[i1])
+                else:
+                    distances_.append(1 + min((distances[i1], distances[i1 + 1], distances_[-1])))
+            distances = distances_
+        return distances[-1]
+    return levenshteinDistance(s1,*args)
+
+def country_iso(country: str="", geo_lkp_file: str=countriesINFO) -> str:
+    """
+
+    converts country str (MUST be in geo_lkp_file["Country"] to language) 
+     to iso string
+
+    :param country: country string    
+    Returns language id spoken in a country
+    """
+
+    #preproc
+    country = country.lower()
+
+    df = pd.read_pickle(lkp_file) 
+
+    index = df.index[df["Country"]==country]
+    country_row = df.iloc[index]
+    languages = country_row["Languages"]
+    try:
+        lang_codes = languages.item() #if succeeds, found exactly one matching country
+    except ValueError:
+        warnings.warn(f"Couldnt find country={country} in country info file={geo_lkp_file}")
+        lang_codes = None #failed, string didnt match any country
+
+    return lang_codes
+
+
+
+def update_trends(proxies:List[str]=["http://179.108.169.71:8080"],topics=topics, countries=countries) -> None:
 
     for topic in topics:
         for country in countries:
